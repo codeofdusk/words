acdb1ef0fc6193ee4818ed7a0f85af9a25635898
Bill Dengler <codeofdusk@gmail.com>
2016-03-29 21:24:26 -0400

    Use Counter instead of dict.

--- words.py
+++ words.py
@@ -1,3 +1,4 @@
+from collections import Counter
 def generate_stripmap():
     "Returns a dictionary (map of keys to values) of punctuation and whitespace characters to be removed from strings. This function uses the constants provided by Python's string module."
     import string
@@ -14,8 +15,6 @@ def generate_stripmap():
 
 def analyze(file,mode=None,stripmap=None):
     "This function analyzes a plane text file, optionally skipping Gutenberg/Wikipedia header and footer, and returns a dictionary (mapping of keys to values) of words to their frequencies. A map of characters to strip from each word may also be provided for efficiency purposes if calling this function multiple times (as we do for this experiment), but if none is provided it will be generated before processing."
-    #Res is an empty dictionary which we will populate with key/value pairs and ultimately return.
-    res={}
     #If we don't have a stripmap, generate one.
     if stripmap==None:
         stripmap=generate_stripmap()
@@ -41,45 +40,20 @@ def analyze(file,mode=None,stripmap=None):
     for word in text.split():
         words.extend(word.translate(stripmap).lower().split())
     #Analyze words, and generate our frequency map.
-    for word in words:
-        res[word]=res.setdefault(word,0)+1
-    return res
-
-def add_dicts(*dicts):
-    "Adds dictionaries in the form returned by analyze."
-    #A dictionary to store the results, we will return this.
-    res={}
-    #Iterate over each dictionary and add its values to res. If more than one dictionary has the same key, perform an addition operation on its values (it is assumed this operation is possible).
-    for dict in dicts:
-        for key,value in dict.items():
-            res[key]=res.setdefault(key,0)+value
-    return res
+    return Counter(words)
 
 def get_top_words(map,max=100,csvpath="out.csv"):
     "Get the most frequently occurring words from a dictionary in the form returned by analyze. By default, it saves its results to out.csv in the current directory, but you may optionally pass a different path. A maximum number of top words to print may also be specified, 100 by default since this experiment will use the top 100 words. Returns None."
-    #Variable to track number of words printed so far
-    count=0
     #Set up csv processing
     import csv
     #Create a file object where the csv data will be written
     fout=open(csvpath,'w')
     #And a writer object to write the data in csv format to the file.
     writer=csv.writer(fout)
-    #First, invert the dictionary. The inverted dictionary will have frequency counts as keys and lists of words as values. We need to do this so we can sort the words by frequency.
-    invmap={}
-    for key,value in map.items():
-        if value not in invmap:
-            invmap[value]=[key]
-        else:
-            invmap[value].append(key)
-    #Sort the keys of the inverted dictionary so the highest frequency is first in the list and the others follow in descending order. Then iterate over the sorted frequencies and track our count.
     writer.writerow(("word","frequency"))
-    for i in sorted(invmap.keys())[::-1]:
-        if count >= max:
-            break
-        for w in invmap[i]:
-            count+=1
-            writer.writerow((w,str(i)))
+    #Use the most_common method on Counter to generate the list, and write to CSV.
+    for word,freq in map.most_common(max):
+        writer.writerow((word,str(freq)))
     fout.close()
 
 if __name__ == '__main__':
@@ -105,18 +79,18 @@ if __name__ == '__main__':
     #Calculate the total number of files to analyze.
     total=len(pgpaths)+len(wppaths)
     print("A total of",total,"files to analyze. Starting Gutenberg analysis...")
-    #Create an empty dictionary to add analyses to.
-    res={}
+    #Create an empty counter to keep track of word frequency counts.
+    res=Counter()
     #And a variable to keep track of how many files analyzed so far
     count=0
     for path in pgpaths:
-        res=add_dicts(res,analyze(path,mode="Gutenberg",stripmap=sm))
+        res+=analyze(path,mode="Gutenberg",stripmap=sm)
         count+=1
         print("Analyzed",count,path)
     print("Finished analyzing Gutenberg. Starting Wikipedia...")
     for path in wppaths:
         print("Analyzing",path)
-        res=add_dicts(res,analyze(path,mode="Wikipedia",stripmap=sm))
+        res+=analyze(path,mode="Wikipedia",stripmap=sm)
         count+=1
         print("Analyzed",count,path)
     print("Done. Generating CSV of top 100 words...")
